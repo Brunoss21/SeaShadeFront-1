aba3bf89b36a4b65d10a124ff32cf62404b0b2ea
brunoss21 <bruno_silva1420@hotmail.com>
2025-10-15 22:32:49 -0300

    Implantação inicial cardapio

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080', // A URL base da sua API
+  baseURL: 'http://localhost:8080',
 });
 
-// Este "interceptor" é uma função que é executada ANTES de cada requisição
 apiClient.interceptors.request.use(
   (config) => {
     // Pega o objeto 'user' do localStorage
@@ -13,8 +12,6 @@ apiClient.interceptors.request.use(
     if (userString) {
       const user = JSON.parse(userString);
       
-      // ATENÇÃO: Use o nome do campo exatamente como ele vem do seu backend.
-      // No nosso último teste, era 'acessToken' (com um 'c' só).
       const token = user.accessToken; 
 
       // Se o token existir, adiciona ao cabeçalho da requisição
